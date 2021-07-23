e8204b9d228a640b926f76c19c1cf2c5bbb32063
ninitto turtle <hanenalrwasheda.off@gmail.com>
2021-07-23 23:05:47 +0200

    added class Timer with simple functionality and acceptable implementation using class-based-components

--- timer/src/Timer.js
+++ timer/src/Timer.js
@@ -1,10 +1,42 @@
 import { Component } from "react";
 
 class Timer extends Component{
+  constructor(){
+    super()
+    this.state = {
+      time : 3,
+      disabled : false
+    }
+    this.decrement = this.decrement.bind(this)
+    this.clicked = this.clicked.bind(this)
+  }
+  stop
+  decrement(){
+    if(this.state.time < 1){
+      clearInterval(this.stop)
+      this.setState({
+        disabled : false,
+        time : 4
+      })
+    }
+    if(this.state.time > 0){
+      this.setState({
+        time : this.state.time - 1
+      })
+    }
+          
+  }
+  clicked(){
+    this.setState({disabled : true})
+    this.stop = setInterval(this.decrement,1000)
+  }
+  
+
   render(){
     return (
       <div>
-        <h1>Welcome In Timer Application</h1>
+        <h1>{this.state.time}</h1>
+        <button onClick={this.clicked} disabled={this.state.disabled}>start</button>
       </div>
     );
   }
